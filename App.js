2e5eabe09a55647257da6348e543bd7f09366e33
mohamadMgahid <52581193+mohamadMgahid@users.noreply.github.com>
2019-09-11 08:27:18 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -14,3 +14,39 @@ searchBar.addEventListener('keyup',function(e){
         }
     })
 });
+
+const hidehotel = document.querySelector('#hide')
+hidehotel.addEventListener('change',function(e){
+    if(hidehotel.checked){
+        list.style.display="none";
+    }else{
+        list.style.display="initial";
+    }
+});
+
+const addForm = document.forms['add-Hotel'];
+
+addForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    const li = document.createElement('li');
+    const hotelName=document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    deleteBtn.textContent = "delete";
+    hotelName.textContent = value;
+
+    hotelName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
+    li.appendChild(hotelName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+});
+
+list.addEventListener('click',function(e){
+    if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        list.removeChild(li);
+    }
+})
